18c32f1e9a37f646155471e1e4a113a7e1f01f06
JT <dd3557@naver.com>
2021-05-18 21:45:46 +0900

    Initial Commit

---
+++ SimpleContact/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleContact
+//
+//  Created by 김진태 on 2021/05/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
